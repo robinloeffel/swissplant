55d71793533a27ba660be7985ce37274a3c8dbca
Robin Loeffel <r.loeffel@hispeed.ch>
2015-06-16 16:58:35 +0200

    general improvements

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,18 @@
+/*global $*/
+
 $("document").ready(function () {
+    "use strict";
     $("[data-scroll-target]").click(function (event) {
         event.preventDefault();
         var scrollTarget = $(event.target).attr("data-scroll-target");
         $("html, body").animate({
             scrollTop: $("#" + scrollTarget).offset().top
+        }, 500);
+    });
+
+    $("#landing i").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#sortiment").offset().top
         }, 500);
     });
 });
