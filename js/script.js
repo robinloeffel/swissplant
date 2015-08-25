98b2cb4799e6ce0e28af4a870cb617c106e10c9c
rdizzle <r.loeffel@hispeed.ch>
2015-08-25 16:48:39 +0200

    major changes

--- js/script.js
+++ js/script.js
@@ -3,60 +3,37 @@
 $("document").ready(function () {
     "use strict";
 
-    var originalNavOffset = $("#landing").outerHeight();
+    var rotateCount = 1;
 
     setInterval(function () {
         rotate();
-    }, 7500);
+    }, 10000);
+
+    function rotate() {
+        switch (rotateCount) {
+        case 1:
+            $(".cover-parent-background.one").removeClass("top");
+            $(".cover-parent-background.two").addClass("top");
+            rotateCount++;
+            break;
+        case 2:
+            $(".cover-parent-background.two").removeClass("top");
+            $(".cover-parent-background.three").addClass("top");
+            rotateCount++;
+            break;
+        case 3:
+            $(".cover-parent-background.three").removeClass("top");
+            $(".cover-parent-background.one").addClass("top");
+            rotateCount = 1;
+            break;
+        }
+    }
 
     $("[data-scroll-target]").click(function (event) {
         event.preventDefault();
         var scrollTarget = $(event.target).attr("data-scroll-target");
         $("html, body").animate({
-            scrollTop: $("#" + scrollTarget).offset().top
+            scrollTop: ($("#" + scrollTarget).offset().top) - (89)
         }, 500);
     });
-
-    $("#landing i").click(function () {
-        $("html, body").animate({
-            scrollTop: $("#sortiment").offset().top
-        }, 500);
-    });
-
-    $(window).on("scroll", function () {
-        var currentPosition = $(window).scrollTop();
-        if (currentPosition > originalNavOffset) {
-            $("nav").css({
-                "position": "fixed",
-                "z-index": 9999,
-                "top": 0,
-                "left": 0
-            });
-            $("#landing").css({
-                "margin-bottom": $("nav").outerHeight()
-            });
-        } else {
-            $("nav").css({
-                "position": "static",
-                "z-index": "auto",
-                "top": "auto",
-                "left": "auto"
-            });
-            $("#landing").css({
-                "margin-bottom": 0
-            });
-        }
-    });
-
-    function rotate() {
-        if (!$("#landing h1").hasClass("hidden")) {
-            $("#landing h1").addClass("hidden").fadeOut("slow", function () {
-                $("#landing img").fadeIn("slow").removeClass("hidden");
-            });
-        } else {
-            $("#landing img").addClass("hidden").fadeOut("slow", function () {
-                $("#landing h1").fadeIn("slow").removeClass("hidden");
-            });
-        }
-    }
 });
---
+++ js/smooth.js
@@ -0,0 +1,3 @@
+(function ($) {
+    $('.smooth').smoothState();
+})(jQuery);
