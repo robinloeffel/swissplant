66d3733b3104c0f617897d5ba819ddb392df512e
Robin Löffel <r.loeffel@hispeed.ch>
2015-08-28 15:24:48 +0200

    layout fixes, new cropped logo in nav

--- js/smooth.js
+++ js/smooth.js
@@ -1,3 +1,15 @@
-(function ($) {
-    $('.smooth').smoothState();
-})(jQuery);
+//(function ($) {
+//    'use strict';
+//    var $body = $('html, body'),
+//        content = $('#main').smoothState({
+//            onStart: {
+//                duration: 250,
+//                render: function (url, $container) {
+//                    content.toggleAnimationClass('is-exiting');
+//                    $body.animate({
+//                        scrollTop: 0
+//                    });
+//                }
+//            }
+//        }).data('smoothState');
+//})(jQuery);
