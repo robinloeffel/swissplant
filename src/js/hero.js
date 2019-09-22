cf0c33ad80beac1c0bb0d497d05e59945c1ad71a
Robin Löffel <r.loeffel@outlook.com>
2019-09-22 15:15:47 +0200

    rotate hero image

---
+++ src/js/hero.js
@@ -0,0 +1,19 @@
+const images = document.getElementsByClassName('_landing-image');
+const visible = '_landing-image-visible';
+let idx = 0;
+
+const rotate = () => {
+  if (idx > 0) {
+    images[idx - 1].classList.remove(visible);
+  }
+
+  if (idx === 0) {
+    images[images.length - 1].classList.remove(visible);
+  }
+
+  images[idx].classList.add(visible);
+  idx = idx === images.length - 1 ? 0 : idx += 1;
+}
+
+setInterval(rotate, 7500);
+rotate();
--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -1,2 +1,1 @@
-import svg4everybody from 'svg4everybody';
-svg4everybody();
+import 'svgxuse';
