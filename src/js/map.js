1430fa7c2fe5fddc14ba6f3b92d27248807a1cca
Robin Löffel <hi@robinloeffel.ch>
2021-05-11 13:24:29 +0200

    feat: load leaflet asynchronously

--- src/js/map.js
+++ src/js/map.js
@@ -1,19 +1,25 @@
-import L from 'leaflet';
+const mapContainer = document.querySelector('._map');
 
-const node = document.querySelector('._map');
+if (mapContainer) {
+  import(
+    /* webpackChunkName: 'leaflet' */
+    'leaflet'
+  ).then(({
+    default: Leaflet
+  }) => {
+    const leafletMap = Leaflet.map(mapContainer);
+    leafletMap.setView([ 46.9902244, 7.1445924 ], 14);
 
-if (node) {
-  window.addEventListener('DOMContentLoaded', () => {
-    const map = L.map(node).setView([ 46.9902244, 7.1445924 ], 14);
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      maxZoom: 24,
-      id: 'mapbox.streets',
-      detectRetina: true,
+    Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Kartendaten © <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>; Bilder © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      detectRetina: true,
       accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
-    }).addTo(map);
+    }).addTo(leafletMap);
 
-    L.marker([ 46.9902244, 7.1445924 ]).addTo(map);
+    Leaflet.marker([ 46.9902244, 7.1445924 ]).addTo(leafletMap);
   });
 }
