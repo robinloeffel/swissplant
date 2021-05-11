60546b426d720950634fc2ec1377a29230cd7dce
Robin Löffel <hi@robinloeffel.ch>
2021-05-11 13:25:05 +0200

    refactor: clean up hero rotation logic

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,21 +1,18 @@
 const images = document.querySelectorAll('._landing-image');
-const visibleClass = '_landing-image-visible';
-let idx = 0;
 
-const rotate = () => {
-  if (idx > 0) {
-    images[idx - 1].classList.remove(visibleClass);
-  }
+if (images.length > 0) {
+  const visibleClass = '_landing-image-visible';
+  let [ currentlyVisible ] = images;
+  currentlyVisible.classList.add(visibleClass);
 
-  if (idx === 0) {
-    images[images.length - 1].classList.remove(visibleClass);
-  }
+  window.setInterval(() => {
+    const hasNextSibling = !!currentlyVisible.parentElement.nextElementSibling;
+    const nextVisible = hasNextSibling
+      ? currentlyVisible.parentElement.nextElementSibling.lastElementChild
+      : images[0];
 
-  images[idx].classList.add(visibleClass);
-  idx = idx === images.length - 1 ? 0 : idx += 1;
-};
-
-if (images.length > 1) {
-  setInterval(rotate, 7500);
-  rotate();
+    currentlyVisible.classList.remove(visibleClass);
+    nextVisible.classList.add(visibleClass);
+    currentlyVisible = nextVisible;
+  }, 7500);
 }
