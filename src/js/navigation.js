e6178314091afd49a00602d27593f3247334f8fc
Robin Löffel <hi@robinloeffel.ch>
2022-11-21 13:12:16 +0100

    fix: improve navigation calculations

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -3,14 +3,13 @@ const list = navigation.querySelector('.navigation-list');
 const logo = navigation.querySelector('.navigation-item-logo');
 const trigger = navigation.querySelector('.navigation-mobile-button');
 
-const listGap = Number(window.getComputedStyle(list).gap.replace('px', ''));
-const logoHeight = logo.getBoundingClientRect().height;
-
-const navigationHeightExpanded = navigation.getBoundingClientRect().height;
-const navigationHeightCollapsed = listGap * 2 + logoHeight;
-
-navigation.style.setProperty('--navigation-height-expanded', `${navigationHeightExpanded}px`);
+const listGap = Number(window.getComputedStyle(list).rowGap.replace('px', ''));
+const navigationHeightCollapsed = logo.clientHeight + listGap * 2;
 navigation.style.setProperty('--navigation-height-collapsed', `${navigationHeightCollapsed}px`);
+
+trigger.addEventListener('click', () => {
+  navigation.style.setProperty('--navigation-height-expanded', `${list.clientHeight}px`);
+}, { once: true });
 
 trigger.addEventListener('click', () => {
   navigation.classList.toggle('navigation--open', !navigation.classList.contains('navigation--open'));
