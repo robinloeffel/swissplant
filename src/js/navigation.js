1c46c72bd3a0ac22113387f54a8a0458278a1671
Robin Löffel <hi@robinloeffel.ch>
2022-11-21 13:55:55 +0100

    feat: show/hide nav bar on scroll

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -7,9 +7,24 @@ const listGap = Number(window.getComputedStyle(list).rowGap.replace('px', ''));
 const navigationHeightCollapsed = logo.clientHeight + listGap * 2;
 navigation.style.setProperty('--navigation-height-collapsed', `${navigationHeightCollapsed}px`);
 
-trigger.addEventListener('click', () => {
+const scrollBuffer = 50;
+let oldScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > oldScrollY + scrollBuffer) {
+    navigation.classList.add('navigation--hidden');
+    oldScrollY = window.scrollY;
+  }
+
+  if (window.scrollY < oldScrollY - scrollBuffer) {
+    navigation.classList.remove('navigation--hidden');
+    oldScrollY = window.scrollY;
+  }
+}, { passive: true });
+
+window.addEventListener('load', () => {
   navigation.style.setProperty('--navigation-height-expanded', `${list.clientHeight}px`);
-}, { once: true });
+});
 
 trigger.addEventListener('click', () => {
   navigation.classList.toggle('navigation--open', !navigation.classList.contains('navigation--open'));
