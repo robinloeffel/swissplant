cb84103888958ea94b6692064870150ccc56b091
Robin Löffel <hi@robinloeffel.ch>
2022-12-25 14:25:07 +0100

    feat: add jobs page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   extends: 'sweet',
   overrides: [{
-    files: [ 'gulpfile.mjs', 'stylelint.config.cjs' ],
+    files: [
+      'gulpfile.mjs',
+      'stylelint.config.cjs',
+      '.eslintrc.cjs'
+    ],
     extends: 'sweet/node',
     rules: {
       'node/no-unsupported-features/es-syntax': 'off'
