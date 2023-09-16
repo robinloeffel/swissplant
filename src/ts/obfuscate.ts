d2cd3bf8d4e6d3b28242f8cd7793903fb5c9c6ba
Robin Löffel <hi@robinloeffel.ch>
2023-09-16 14:36:32 +0200

    refactor: use a map, add new values

--- src/ts/obfuscate.ts
+++ src/ts/obfuscate.ts
@@ -1,5 +1,44 @@
 const obfuscated = document.querySelectorAll<HTMLAnchorElement>("[data-obfuscate]");
 
+const pairs = new Map([
+  [ "bWFpbHRvOmluZm9Ac3dpc3NwbGFudC5jaA==", {
+    attribute: "obfuscateEsp",
+    text: "SwissPlant eine E-Mail schreiben"
+  }],
+  [ "bWFpbHRvOmluZm9AYWdyaXBsYW50LmNo", {
+    attribute: "obfuscateEap",
+    text: "AgriPlant eine E-Mail schreiben"
+  }],
+  [ "bWFpbHRvOm0ubG9lZmZlbEBzd2lzc3BsYW50LmNo", {
+    attribute: "obfuscateEml",
+    text: "Martin Löffel eine E-Mail schreiben"
+  }],
+  [ "bWFpbHRvOnQuYmF1bWFubkBzd2lzc3BsYW50LmNo", {
+    attribute: "obfuscateEtb",
+    text: "Telja Baumann eine E-Mail schreiben"
+  }],
+  [ "bWFpbHRvOnMubG9lZmZlbEBzd2lzc3BsYW50LmNo", {
+    attribute: "obfuscateEsl",
+    text: "Sandro Löffel eine E-Mail schreiben"
+  }],
+  [ "dGVsOiArNDEgMzIgMzEzIDUyIDEw", {
+    attribute: "obfuscateT",
+    text: "SwissPlant anrufen"
+  }],
+  [ "dGVsOis0MTc5NjMxNDU1OQ==", {
+    attribute: "obfuscateTml",
+    text: "Martin Löffel anrufen"
+  }],
+  [ "dGVsOis0MTc5ODE1MzgzNQ==", {
+    attribute: "obfuscateTtb",
+    text: "Telja Baumann anrufen"
+  }],
+  [ "dGVsOis0MTc5NjQxNTIxMA==", {
+    attribute: "obfuscateTsl",
+    text: "Sandro Löffel anrufen"
+  }]
+]);
+
 const sr = (link: HTMLAnchorElement, text: string) => {
   const srTextElement = document.createElement("span");
   srTextElement.classList.add("visually-hidden");
@@ -15,22 +54,11 @@ const handleClick = (event: MouseEvent) => {
 };
 
 for (const obfuscatedElement of obfuscated) {
-  const obfuscatedText = obfuscatedElement.dataset.obfuscate!;
+  const pair = pairs.get(obfuscatedElement.dataset.obfuscate!);
 
-  if (obfuscatedText === "bWFpbHRvOmluZm9Ac3dpc3NwbGFudC5jaA==") {
-    obfuscatedElement.dataset.obfuscateEsp = "";
-    sr(obfuscatedElement, "SwissPlant eine E-Mail schreiben");
+  if (pair) {
+    obfuscatedElement.addEventListener("click", handleClick);
+    obfuscatedElement.dataset[pair.attribute] = "";
+    sr(obfuscatedElement, pair.text);
   }
-
-  if (obfuscatedText === "bWFpbHRvOmluZm9AYWdyaXBsYW50LmNo") {
-    obfuscatedElement.dataset.obfuscateEap = "";
-    sr(obfuscatedElement, "AgriPlant eine E-Mail schreiben");
-  }
-
-  if (obfuscatedText === "dGVsOiArNDEgMzIgMzEzIDUyIDEw") {
-    obfuscatedElement.dataset.obfuscateT = "";
-    sr(obfuscatedElement, "SwissPlant anrufen");
-  }
-
-  obfuscatedElement.addEventListener("click", handleClick);
 }
