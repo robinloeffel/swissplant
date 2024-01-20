1f54dc87924d2d75f1eed5371c41e09ae5707df6
Robin Löffel <hi@robinloeffel.ch>
2024-01-20 20:18:04 +0100

    fix: set aria-hidden attribute on svg sprite sheet

--- src/ts/spritesheet.ts
+++ src/ts/spritesheet.ts
@@ -4,4 +4,5 @@ const spriteAsText = await response.text();
 
 const { documentElement: spriteElement } = domParser.parseFromString(spriteAsText, "image/svg+xml");
 spriteElement.classList.add("visually-hidden");
+spriteElement.setAttribute("aria-hidden", "true");
 document.body.append(spriteElement);
