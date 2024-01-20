c39be3d880bb665804e416183ea54aaf8f7598ba
Robin Löffel <hi@robinloeffel.ch>
2024-01-20 20:07:02 +0100

    feat: clean up and fix navigation

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,25 +1,42 @@
-const navigation = document.querySelector<HTMLElement>(".navigation");
-const trigger = navigation?.querySelector<HTMLButtonElement>(".navigation-mobile-button");
+import { signal, effect } from "@preact/signals-core";
+
+const navigation = document.querySelector<HTMLElement>("[data-module~=navigation]")!;
+const toggle = navigation.querySelector<HTMLButtonElement>("[data-navigation~=toggle]")!;
 const scrollBuffer = 50;
+const isOpen = signal(false);
 
 let oldScrollY = window.scrollY;
+
+effect(() => {
+  navigation.classList.toggle("open", isOpen.value);
+  toggle.setAttribute("aria-expanded", String(isOpen.value));
+});
 
 // eslint-disable-next-line github/prefer-observers -- no way to do that here
 window.addEventListener("scroll", () => {
   if (window.scrollY > oldScrollY + scrollBuffer) {
-    navigation?.classList.add("navigation--hidden");
+    navigation?.classList.add("hidden");
     oldScrollY = window.scrollY;
   }
 
   if (window.scrollY < oldScrollY - scrollBuffer) {
-    navigation?.classList.remove("navigation--hidden");
+    navigation?.classList.remove("hidden");
     oldScrollY = window.scrollY;
   }
 });
 
-trigger?.addEventListener("click", () => {
-  navigation?.classList.toggle(
-    "navigation--open",
-    !navigation.classList.contains("navigation--open")
-  );
+window.addEventListener("keydown", ({ key }) => {
+  if (isOpen.value && key === "Escape") {
+    isOpen.value = false;
+  }
+});
+
+window.addEventListener("click", ({ target }) => {
+  if (isOpen.value && !navigation.contains(target as Element)) {
+    isOpen.value = false;
+  }
+});
+
+toggle.addEventListener("click", () => {
+  isOpen.value = !isOpen.value;
 });
