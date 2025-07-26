d03a783463c345e71ab0f6919793bb35f81a910e
Robin Löffel <hi@robinloeffel.ch>
2025-07-26 13:05:43 +0200

    fix: don't redirect asset requests

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -2,7 +2,9 @@ import { config } from "$config";
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = ({ cookies }) => {
-  const language = cookies.get("lang") ?? config.defaultLanguage;
-  redirect(307, `/${language}`);
+export const load: PageServerLoad = ({ cookies, url }) => {
+  if (!url.pathname.includes("immutable/") || !url.pathname.includes("assets/")) {
+    const language = cookies.get("lang") ?? config.defaultLanguage;
+    redirect(307, `/${language}`);
+  }
 };
